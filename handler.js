49fad64d067e3405b7c8af962aee43ffbf93a954
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-11-23 14:36:37 +0100

    setup eslint

--- handler.js
+++ handler.js
@@ -1,26 +1,27 @@
+/* eslint-env node */
 'use strict';
 
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 const client = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'example@gmail.com',
-        pass: 'app-password'
-    }
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'example@gmail.com',
+    pass: 'app-password'
+  }
 });
 
 module.exports.hello = (event, context, callback) => {
   let mailOptions = {
     to: 'info@simplabs.com',
-    subject: 'Message from ' + event.name + ' regarding simplabs.com',
-    text: event.message + '\n \n Answer to: ' + event.email
+    subject: `Message from ${event.name} regarding simplabs.com`,
+    text: `${event.message} '\n \n Answer to: ${event.email}`
   };
 
-  client.sendMail(mailOptions, (error, info) => {
+  client.sendMail(mailOptions, (error) => {
     if (error) {
       callback(error);
     }
