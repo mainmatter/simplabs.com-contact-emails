0cfc2d959c51fc13f7b7c3ed34b6664b382f2afe
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-11-23 15:32:04 +0100

    read email user from env

--- handler.js
+++ handler.js
@@ -1,16 +1,16 @@
 /* eslint-env node */
 'use strict';
 
-require('dotenv').config();
 const nodemailer = require('nodemailer');
+
 const client = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
   auth: {
-    user: 'example@gmail.com',
-    pass: 'app-password'
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
   }
 });
 
@@ -18,7 +18,7 @@ module.exports.hello = (event, context, callback) => {
   let mailOptions = {
     to: 'info@simplabs.com',
     subject: `Message from ${event.name} regarding simplabs.com`,
-    text: `${event.message} '\n \n Answer to: ${event.email}`
+    text: `${event.message} '\n \n Answer to: ${event.email}`,
   };
 
   client.sendMail(mailOptions, (error) => {
